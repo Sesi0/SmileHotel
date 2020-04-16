27d575bf6729440f4bc95f40dc7e5258068a0bb7
Guru 2.0 <nikolay.staykov98@abv.bg>
2020-04-17 02:00:51 +0300

    Initial User sql integration

--- SmileHotel/Repositories/UserRepository.cs
+++ SmileHotel/Repositories/UserRepository.cs
@@ -1,15 +1,32 @@
 ﻿using System.Collections.Generic;
 using SmileHotel.Models;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace SmileHotel.sRepositories
 {
     public class UserRepository
     {
+        string connetionString = @"Data Source=remotemysql.com;Initial Catalog=8wdwbHzKHP;User ID=8wdwbHzKHP;Password=kPRONAzKzw";
+        SqlConnection cnn;
+        string query;
+        SqlDataReader DataReader;
         public List<User> GetAllUsers()
         {
             var users = new List<User>();
 
             // TODO: Get users from DB
+            query = "SELECT * FROM Users";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            SqlCommand SqlQuery = new SqlCommand(query, cnn);
+            DataReader = SqlQuery.ExecuteReader();
+            while(DataReader.Read())
+            {
+                User ToAdd = new User();
+                users.Add(ToAdd);
+            }
+            cnn.Close();
 
             return users;
         }
@@ -19,7 +36,13 @@ namespace SmileHotel.sRepositories
             var user = new User();
 
             // TODO: Get user from DB
-
+            query = "SELECT * FROM Users WHERE ID = " + id.ToString();
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            SqlCommand SqlQuery = new SqlCommand(query, cnn);
+            DataReader = SqlQuery.ExecuteReader();
+            DataReader.Read();
+            cnn.Close();
             return user;
         }
 
